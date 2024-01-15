b8a7608e232176a0a8cbde81711c67a4de7bcf5c
Harshil Shah <harshilshah1910@outlook.com>
2024-01-15 13:06:01 +0530

    Initial Commit

---
+++ PeakLocalisations/PeakLocalisationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeakLocalisationsApp.swift
+//  PeakLocalisations
+//
+//  Created by Harshil Shah on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PeakLocalisationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
