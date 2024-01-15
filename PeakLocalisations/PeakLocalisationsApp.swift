af2e885172c3a14acbe3944c7b95c84c12a328b1
Harshil Shah <harshilshah1910@outlook.com>
2024-01-15 13:15:52 +0530

    Added xcstrings files

--- PeakLocalisations/PeakLocalisationsApp.swift
+++ PeakLocalisations/PeakLocalisationsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PeakLocalisationsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Text(verbatim: "Hello, world!")
         }
     }
 }
